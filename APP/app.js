b17e478f575293520fa5cf09aceb830946f3f59a
ethereal element <vitalik.safonov@list.ru>
2023-04-14 07:49:19 +0300

    check commit

---
+++ APP/app.js
@@ -0,0 +1,6 @@
+"use strict";
+import { add,mult } from "./calc";
+
+console.log(add(1,2));
+console.log(mult(3,4));
+// const a = 1;
---
+++ APP/calc.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const a = 1;
+
+export function add(a,b) {
+	return a + b
+}
+
+export function mult(s,v) { 
+	return s * v 
+}
+
