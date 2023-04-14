5528131f244ceb78679ec14eb88c18fb7b8665c5
ethereal element <vitalik.safonov@list.ru>
2023-04-14 11:49:08 +0300

    created header component

--- APP/dist/app.js
+++ APP/dist/app.js
@@ -1025,22 +1025,37 @@
   	}
   }
 
-  class Header extends DivComponent{ 
-  	constructor(appState) {
-  		super();
-  		this.appState = appState;
-  	}
+  class Header extends DivComponent {
+    constructor(appState) {
+      super();
+      this.appState = appState;
+    }
 
-  	render() {
-  		this.el.innerHTML = "";
-  		this.el.classList.add("header");
-  		this.el.innerHTML = `
+    render() {
+      this.el.innerHTML = "";
+      this.el.classList.add("header");
+      this.el.innerHTML = `
 		<div>
 			<img src="/static/logo.svg" alt="logo"/>
 		</div>
+
+		<div class="menu">
+		<a class="menu__item" href="#">
+		<img src="/static/search.svg" alt="Search"/>
+		Поиск книг
+		</a>
+
+		<a class="menu__item" href="#favorites">
+		<img src="/static/favorites.svg" alt="Favorite"/>
+		Избранное
+		<div class="menu__counter">
+				${this.appState.favorites.length}
+		</div>
+		</a>
+		</div>
 		`;
-  		return this.el
-  	}
+      return this.el;
+    }
   }
 
   class MainView extends AbstractView {
--- APP/src/components/header/header.js
+++ APP/src/components/header/header.js
@@ -1,19 +1,36 @@
 import { DivComponent } from "../../common/div-component";
+import "./header.css"
 
-export class Header extends DivComponent{ 
-	constructor(appState) {
-		super();
-		this.appState = appState;
-	}
 
-	render() {
-		this.el.innerHTML = "";
-		this.el.classList.add("header");
-		this.el.innerHTML = `
+export class Header extends DivComponent {
+  constructor(appState) {
+    super();
+    this.appState = appState;
+  }
+
+  render() {
+    this.el.innerHTML = "";
+    this.el.classList.add("header");
+    this.el.innerHTML = `
 		<div>
 			<img src="/static/logo.svg" alt="logo"/>
 		</div>
+
+		<div class="menu">
+		<a class="menu__item" href="#">
+		<img src="/static/search.svg" alt="Search"/>
+		Поиск книг
+		</a>
+
+		<a class="menu__item" href="#favorites">
+		<img src="/static/favorites.svg" alt="Favorite"/>
+		Избранное
+		<div class="menu__counter">
+				${this.appState.favorites.length}
+		</div>
+		</a>
+		</div>
 		`;
-		return this.el
-	}
+    return this.el;
+  }
 }
